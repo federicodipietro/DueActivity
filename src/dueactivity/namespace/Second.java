c8f1305f92227f962a11b37d32cd13c0204aa95c
mio <io@ubuntu.ubuntu-domain>
2012-01-18 15:09:55 +0100

    secondo progetto

---
+++ src/dueactivity/namespace/Second.java
@@ -0,0 +1,13 @@
+package dueactivity.namespace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Second extends Activity{
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.second);
+	    }
+}
